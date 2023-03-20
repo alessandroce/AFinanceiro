5f84c27b49ae249604d19f50bc967184dcf3aad0
Alessandro <30908676+alessandroce@users.noreply.github.com>
2023-03-19 21:51:27 -0300

    - Ajustado SQL para retornar Limite

--- Script/PR_RETORNA_LIMITE.sql
+++ Script/PR_RETORNA_LIMITE.sql
@@ -5,14 +5,13 @@ returns (
 as
 BEGIN
   --
-  SELECT FIRST 1
-         L.LIM_VALOR
-    FROM FINANCEIRO        F
-       , FINANCEIRO_LIMITE L
-   WHERE 1=1
-     AND F.FIN_ID = L.LIM_FIN_ID
-     AND F.FIN_ID = :N_FINANC
-  ORDER BY L.LIM_DATA DESC
+  SELECT FL.LIM_VALOR
+    FROM FINANCEIRO_LIMITE FL
+   WHERE FL.LIM_DATA = (SELECT MAX(LM.LIM_DATA) --vigencia
+                          FROM FINANCEIRO_LIMITE LM
+                         WHERE LM.LIM_FIN_ID = :N_FINANC
+                           AND LM.LIM_DATA  <= CURRENT_DATE --sem data futura
+                           AND LM.LIM_FIN_ID = FL.LIM_FIN_ID)
   INTO N_LIMITE;
   --
   SUSPEND;
